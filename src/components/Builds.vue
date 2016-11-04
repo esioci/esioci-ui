6b1963d9978caccac3d42d1e3f8c375042f70c74
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2016-11-04 18:02:27 +0100

    Fix Builds unit tests

--- src/components/Builds.vue
+++ src/components/Builds.vue
@@ -1,37 +1,39 @@
 <template>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      Builds list
-    </div>
-    <div class="panel-body">
-      <table class="table table-hover">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>status</th>
-            <th>build start</th>
-            <th>last update</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="build in builds" v-bind:class="{
-            'success': build.state === 'COMPLETED',
-            'warning': build.state === 'RUNNING',
-            'danger': build.state === 'FAILED'
-          }">
-            <td>{{ build.id }}</td>
-            <td><span v-bind:class="{"glyphicon glyphicon-refresh glyphicon-spin": build.state === "RUNNING"}"></span> {{ build.state }}</td>
-            <td>{{ build.inserted_at }}</td>
-            <td>{{ build.updated_at }}</td>
-<!--             <td><a v-bind:href="{{ build.id }}/build_{{ build.id }}.txt"><span class="glyphicon glyphicon-file" title="get build.log"></span></a></td>
- -->            
-            <td><a v-bind:href="url"></a>
-            <td><a href="#"><span class="glyphicon glyphicon-floppy-save" title="get artifacts"></span></a></td>
-          </tr>
-        </tbody>
-      </table>
+  <div id="builds">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        Builds list
+      </div>
+      <div class="panel-body">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>status</th>
+              <th>build start</th>
+              <th>last update</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="build in builds" v-bind:class="{
+              'success': build.state === 'COMPLETED',
+              'warning': build.state === 'RUNNING',
+              'danger': build.state === 'FAILED'
+            }">
+              <td>{{ build.id }}</td>
+              <td><span v-bind:class="{"glyphicon glyphicon-refresh glyphicon-spin": build.state === "RUNNING"}"></span> {{ build.state }}</td>
+              <td>{{ build.inserted_at }}</td>
+              <td>{{ build.updated_at }}</td>
+  <!--             <td><a v-bind:href="{{ build.id }}/build_{{ build.id }}.txt"><span class="glyphicon glyphicon-file" title="get build.log"></span></a></td>
+   -->            
+              <td><a href="#"></a>
+              <td><a href="#"><span class="glyphicon glyphicon-floppy-save" title="get artifacts"></span></a></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
--- test/unit/Builds.spec.js
+++ test/unit/Builds.spec.js
@@ -1,23 +1,22 @@
-// import Vue from 'vue'
-// import Builds from '../../src/components/Builds.vue'
+import Vue from 'vue'
+import Builds from '../../src/components/Builds.vue'
 
 describe('Builds.vue', () => {
-  var b = require('../../src/components/Builds.vue')
+  it('should render correct contents', () => {
+    const vm = new Vue({
+      el: document.createElement('div'),
+      render: (h) => h(Builds)
+    })
+    console.log(vm);
+    expect(vm.$el.querySelector('div.panel-heading').textContent).toMatch('Builds list')
+  });
+
+  var b = require('../../src/components/Builds.vue');
   it('should have data', function () {
     var defaultData = b.data();
-    // var builds = { id: "", state: "", inserted_at: "", updated_at: ""};
     var builds = Object({ builds: [ Object({ id: '', state: '', inserted_at: '', updated_at: '' }) ] })
-    //var builds = [{ id: '', state: '', inserted_at: '', updated_at: '' }];
     expect(defaultData).toEqual(builds);
-    // body...
-  })
-  // it('should render correct contents', () => {
-  //   const vm = new Vue({
-  //     el: document.createElement('div'),
-  //     render: (h) => h(builds)
-  //   })
-  //   expect(vm.$el.querySelector('panel-heading').textContent).toBe('Builds List')
-  // })
+  });
 })
 
 // also see example testing a component with mocks at
